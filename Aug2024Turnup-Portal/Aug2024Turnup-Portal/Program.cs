23e38be51d8c46fdf3686a6d7cd2d97d4262680f
sridevi <sri.kisira@gmail.com>
2024-08-19 17:33:25 +1000

    added login test

---
+++ Aug2024Turnup-Portal/Aug2024Turnup-Portal/Program.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Xml.Serialization;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        // Open Chrome Browser
+        IWebDriver driver = new ChromeDriver();
+
+        // Lanunch TurnnabUp Portal
+        driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
+        driver.Manage().Window.Maximize();
+        // Identify username Textbox and enter valid username
+        IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+        usernameTextbox.SendKeys("hari");
+        // Identify Password textbox and enter valid password 
+        IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+        passwordTextbox.SendKeys("123123");
+        //identify login button and click on it
+        IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+        loginButton.Click();
+
+        //check if the User has logged in
+        IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+        if (helloHari.Text == "Hello hari!")
+        {
+            Console.WriteLine("I can see Hari is logged in");
+        }
+        else
+        {
+            Console.WriteLine("User is not logged in");
+        }
+    }   
+    
+               
+} 
+
+      
