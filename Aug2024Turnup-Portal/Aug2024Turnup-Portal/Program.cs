338090322c3229d7640c5454bfc1cc193ef2088b
sridevi <sri.kisira@gmail.com>
2024-08-29 12:01:36 +1000

    Login and create Time Record automated tests build

--- Aug2024Turnup-Portal/Aug2024Turnup-Portal/Program.cs
+++ Aug2024Turnup-Portal/Aug2024Turnup-Portal/Program.cs
@@ -1,40 +1,294 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Net;
 using System.Xml.Serialization;
 
 public class Program
 {
     private static void Main(string[] args)
     {
+
         // Open Chrome Browser
         IWebDriver driver = new ChromeDriver();
-
         // Lanunch TurnnabUp Portal
         driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
         driver.Manage().Window.Maximize();
+        Thread.Sleep(1000);
+
         // Identify username Textbox and enter valid username
-        IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
-        usernameTextbox.SendKeys("hari");
+        IWebElement userNameTextbox = driver.FindElement(By.Id("UserName"));
+        userNameTextbox.SendKeys("hari");
+
         // Identify Password textbox and enter valid password 
         IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
         passwordTextbox.SendKeys("123123");
+
         //identify login button and click on it
         IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
         loginButton.Click();
+        Thread.Sleep(2000);
 
-        //check if the User has logged in
-        IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
-        if (helloHari.Text == "Hello hari!")
+        //check if the User has logged in successfully
+        IWebElement hellohari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+        if (hellohari.Text == "Hello hari!")
         {
-            Console.WriteLine("I can see Hari is logged in");
+            Console.WriteLine("user has logged n successfully.Test passed!");
         }
         else
         {
-            Console.WriteLine("User is not logged in");
+            Console.WriteLine("User is not logged in.Test failed!");
         }
-    }   
-    
-               
-} 
 
-      
+        // Create a Time record
+
+        // Navigate to Time and material page
+        IWebElement administrationTab = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a/span"));
+        administrationTab.Click();
+
+        IWebElement timeAndMaterialOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+        timeAndMaterialOption.Click();
+
+
+        // Click on create New Button
+        IWebElement CreateNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+        CreateNewButton.Click();
+
+        // Select Time from Dropdown
+        IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
+        typeCodeDropdown.Click();
+
+        IWebElement timeOptions = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
+        timeOptions.Click();
+        Thread.Sleep(1000);
+
+        // Type code into code textbox
+        IWebElement CodeTextbox = driver.FindElement(By.Id("Code"));
+        CodeTextbox.SendKeys("TA Programme");
+
+        // Type description into Description testbox
+        IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+        descriptionTextbox.SendKeys("This is a description");
+
+        //  Type price into price textbox
+        IWebElement PriceTagOverLap = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+        PriceTagOverLap.Click();
+
+        IWebElement PriceTextbox = driver.FindElement(By.Id("Price"));
+        PriceTextbox.SendKeys("12");
+
+        // Click on Save button
+        IWebElement SaveButton = driver.FindElement(By.Id("SaveButton"));
+        SaveButton.Click();
+        Thread.Sleep(3000);
+
+        //  Check if Time record has been created successfully  
+        IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+        goToLastPageButton.Click();
+        Thread.Sleep(2000);
+
+        IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+        if (newCode.Text == "TA Programme")
+        {
+            Console.WriteLine("time record created successfully");
+        }
+        else
+        {
+            Console.WriteLine("new time record has not been created!");
+        }
+    }
+  }
+        
+
+           
+                
+        
+
+
+
+
+
+        
+
+
+           
+        
+        
+
+        
+
+            
+
+
+                            
+        
+
+          
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+
+     
+
+
+
+        
+
+        
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
